32adef314a1a3e3ec21cf4081a06db3edcd4775f
Jerry Ehimuh <jeehimuh@gmail.com>
2025-08-17 22:01:32 -0500

    updated jerry pfp

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/pfp_logo.png"
+              src="/jerry.jpg"
               alt="Jerry portrait"
               width="192"
               height="192"
